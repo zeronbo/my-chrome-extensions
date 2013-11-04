cad9ae8d139f1a22a7c0ecb2470a58ec77920b4f
Zero <zeronbo@163.com>
2013-11-04 13:14:14 +0800

    初始化

---
+++ my_clock/js/my_clock.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * User: Zero
+ * Date: 2013-11-04 12:36
+ *
+ */
+function my_clock(el) {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = m >= 10 ? m : ('0' + m);
+	s = s >= 10 ? s : ('0' + s);
+	el.innerHTML = h + ':' + m + ':' + s;
+	setTimeout(function() {my_clock(el)}, 1000);
+}
+
+var clock_div = document.getElementById('clock_div');
+my_clock(clock_div);
